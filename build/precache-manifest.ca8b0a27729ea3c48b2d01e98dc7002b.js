23f07eac531e123689088dde9822a30a3965c41a
trnet4334 <search0210@gmail.com>
2021-12-09 13:20:45 +0800

    Upgrade react-scripts packages to fix react-router-dom build bug.

---
+++ build/precache-manifest.ca8b0a27729ea3c48b2d01e98dc7002b.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = [
+  {
+    "revision": "34c9db2dd07234133e73",
+    "url": "/static/css/main.0c01227b.chunk.css"
+  },
+  {
+    "revision": "34c9db2dd07234133e73",
+    "url": "/static/js/main.8ef9a114.chunk.js"
+  },
+  {
+    "revision": "791881373abfa2235a7e",
+    "url": "/static/js/runtime~main.bcd1a6c2.js"
+  },
+  {
+    "revision": "1a01947cc4092a2f2930",
+    "url": "/static/js/2.af6a745c.chunk.js"
+  },
+  {
+    "revision": "abd98e3345374acec5f8",
+    "url": "/static/js/3.c223ef96.chunk.js"
+  },
+  {
+    "revision": "0e98edf99dac3bece70e354f1dd8779e",
+    "url": "/static/media/barber-img-1.0e98edf9.jpeg"
+  },
+  {
+    "revision": "b0ef8d180247b0b6092c171ee29d00bb",
+    "url": "/static/media/barber-img-4.b0ef8d18.jpeg"
+  },
+  {
+    "revision": "bc365c18d706d60932f047d0d568c508",
+    "url": "/static/media/barber-img-2.bc365c18.jpeg"
+  },
+  {
+    "revision": "ebf24919cf84235211e982b37ea79397",
+    "url": "/static/media/news-img-1.ebf24919.jpeg"
+  },
+  {
+    "revision": "9eac010516ba313927168961c2199819",
+    "url": "/static/media/barber-img-3.9eac0105.jpeg"
+  },
+  {
+    "revision": "c3f1387b63a908754325944edd4839d5",
+    "url": "/static/media/news-img-3.c3f1387b.jpeg"
+  },
+  {
+    "revision": "0784cfdf2ddd06d4e48adf36ce33cf22",
+    "url": "/static/media/barber-img-6.0784cfdf.jpeg"
+  },
+  {
+    "revision": "d3c6349b465d3e9982b53b2497db1548",
+    "url": "/static/media/barber-img-5.d3c6349b.jpeg"
+  },
+  {
+    "revision": "008b0692e9aba0052647414855e78401",
+    "url": "/static/media/news-img-2.008b0692.jpeg"
+  },
+  {
+    "revision": "e4c4100f559e2c1285396be5562b7622",
+    "url": "/static/media/news-img-5.e4c4100f.jpeg"
+  },
+  {
+    "revision": "32243b4bd6431461ac9417efd68f5ac6",
+    "url": "/static/media/news-img-4.32243b4b.jpeg"
+  },
+  {
+    "revision": "fe801633c07c62e1f98256c8f8dbfe37",
+    "url": "/static/media/news-img-6.fe801633.jpeg"
+  },
+  {
+    "revision": "001c6fa9b6ee95d7327e23441929f832",
+    "url": "/static/media/background-img-2.001c6fa9.png"
+  },
+  {
+    "revision": "f84277b1c13bea98be7170595b162998",
+    "url": "/static/media/about-img-2.f84277b1.jpeg"
+  },
+  {
+    "revision": "cd2c05e26bed594d2f76df3e2eed68c1",
+    "url": "/static/media/about-img-1.cd2c05e2.jpeg"
+  },
+  {
+    "revision": "57bd0083812a612518443fe69abc497b",
+    "url": "/static/media/about-img-3.57bd0083.jpeg"
+  },
+  {
+    "revision": "da215cccd8c2cfb6a814e0fae896afea",
+    "url": "/static/media/about-img-4.da215ccc.jpeg"
+  },
+  {
+    "revision": "5e2ea26e3b72df1a9aabc4456aa2c954",
+    "url": "/static/media/about-img-6.5e2ea26e.jpeg"
+  },
+  {
+    "revision": "bfad0f58e4bdbfb88dcb6d0011393aea",
+    "url": "/static/media/about-img-5.bfad0f58.jpeg"
+  },
+  {
+    "revision": "de95122cf5229f8b3ea8fbb71ea7f526",
+    "url": "/static/media/gallery-img-1.de95122c.jpeg"
+  },
+  {
+    "revision": "1fb7cfb05f4e5241aa1070345efed0a8",
+    "url": "/static/media/gallery-img-4.1fb7cfb0.jpeg"
+  },
+  {
+    "revision": "863bbaba4f437b09b180d403ddabffa6",
+    "url": "/static/media/gallery-img-3.863bbaba.jpeg"
+  },
+  {
+    "revision": "8822242032fc6d6479f272a4bd2cea09",
+    "url": "/static/media/gallery-img-2.88222420.jpeg"
+  },
+  {
+    "revision": "21c2b42324026a8d6864bc1304a47c17",
+    "url": "/static/media/gallery-img-5.21c2b423.jpeg"
+  },
+  {
+    "revision": "f7ad89df6556129b7eeafe5c7a8c73c8",
+    "url": "/static/media/gallery-img-7.f7ad89df.jpeg"
+  },
+  {
+    "revision": "efa42a9959a3d98814ca57b3574e02c8",
+    "url": "/static/media/gallery-img-6.efa42a99.jpeg"
+  },
+  {
+    "revision": "ea45a3309f3f7649cede7f2f17f654df",
+    "url": "/static/media/gallery-img-8.ea45a330.jpeg"
+  },
+  {
+    "revision": "29b8d28703fa64016d7056e4545b5782",
+    "url": "/static/media/gallery-img-10.29b8d287.jpeg"
+  },
+  {
+    "revision": "8fe92a430f92a6af1a5e6ec69694d3a5",
+    "url": "/static/media/gallery-img-9.8fe92a43.jpeg"
+  },
+  {
+    "revision": "41d4fb60e0b99adbc9747bdb5d220937",
+    "url": "/static/media/404-img.41d4fb60.png"
+  },
+  {
+    "revision": "3b90589545de6484172334d244d39c8e",
+    "url": "/static/media/reservation-img.3b905895.jpeg"
+  },
+  {
+    "revision": "0f213ccd389eff8c336776a713d6f704",
+    "url": "/static/media/header-banner.0f213ccd.jpeg"
+  },
+  {
+    "revision": "14db636bd285fdd96585e80542f48925",
+    "url": "/static/media/protocol-img-1.14db636b.png"
+  },
+  {
+    "revision": "965ce7c4c0f538d259ca0d0f6b78a364",
+    "url": "/static/media/header-img-1.965ce7c4.jpeg"
+  },
+  {
+    "revision": "5185434c20e65b69a0daa9ef7b804ca1",
+    "url": "/static/media/work-img-1.5185434c.jpeg"
+  },
+  {
+    "revision": "7fa2c54b06d272a57932e3e7bcf9091b",
+    "url": "/static/media/work-img-2.7fa2c54b.jpeg"
+  },
+  {
+    "revision": "648bb5e35c81a68555bf9b3ac8f111b0",
+    "url": "/static/media/background-img.648bb5e3.png"
+  },
+  {
+    "revision": "4063061d9653a9ae536d2f577de44863",
+    "url": "/static/media/team-img-1.4063061d.jpeg"
+  },
+  {
+    "revision": "05a2d753c63df06d07b60c4bcfaeae9c",
+    "url": "/static/media/work-img-3.05a2d753.jpeg"
+  },
+  {
+    "revision": "0b58d5cbf25eea6e85d99bbe508e8d5d",
+    "url": "/static/media/work-img-4.0b58d5cb.jpeg"
+  },
+  {
+    "revision": "877346f69a46e1cfb2e91e5e771e4497",
+    "url": "/static/media/service-img.877346f6.jpeg"
+  },
+  {
+    "revision": "856de5d975793834fa113bacc28d2fdb",
+    "url": "/static/media/header-img-main.856de5d9.jpeg"
+  },
+  {
+    "revision": "ed92f78086e7fb136c2adc47c021e8e5",
+    "url": "/static/media/work-bg.ed92f780.jpeg"
+  },
+  {
+    "revision": "72de223a85d4a26509345bbaf295c0b5",
+    "url": "/index.html"
+  }
+];
